bc2b89298b7d5be9f74dd0bbb1e33887fd0b32d5
Vitor <37679778+vtrsnts@users.noreply.github.com>
2021-08-10 02:47:32 -0300

    Add files via upload

---
+++ ClientApp/build/precache-manifest.94b30dceb2adffa96c8d1532d9b75fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3aff9f993524ae2c16e1502d19a1a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d6bddc2585ca615c7d",
+    "url": "/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "fd4f82150dcf1925eaa6",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "48d6bddc2585ca615c7d",
+    "url": "/static/js/2.b88dec54.chunk.js"
+  },
+  {
+    "revision": "83e9426a94f2d019bb00e89f0b2c5263",
+    "url": "/static/js/2.b88dec54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4f82150dcf1925eaa6",
+    "url": "/static/js/main.1af3b43d.chunk.js"
+  },
+  {
+    "revision": "163c35f37c7310666141",
+    "url": "/static/js/runtime-main.ca4b5fc4.js"
+  }
+]);
