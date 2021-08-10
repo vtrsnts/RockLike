4877e9ce1d96d14012b7b99463d8893e797c07fe
Vitor <37679778+vtrsnts@users.noreply.github.com>
2021-08-10 02:45:41 -0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RockLike.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockLike.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+        public DbSet<SiteLike> SiteLike { get; set; }
+        public DbSet<Site> Site { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<SiteLike>()
+                   .HasKey(c => new { c.IdSite, c.IdUser });
+
+            builder.Entity<SiteLike>()
+                   .HasOne(bc => bc.Site)
+                   .WithMany(b => b.ListUserSiteLike)
+                   .HasForeignKey(bc => bc.IdSite);
+
+            builder.Entity<SiteLike>()
+                   .HasOne(bc => bc.User)
+                   .WithMany(c => c.ListaSiteLike)
+                   .HasForeignKey(bc => bc.IdUser);
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
